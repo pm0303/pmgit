eba41d27e6a8d5c257a77848806c974df981bd7f
pm0303 <2641938505@qq.com>
2020-02-11 14:39:20 +0800

    创建项目 创建应用

---
+++ end/project/bookdemo/booktest/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# Django自带的后台管理
---
+++ end/project/bookdemo/booktest/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+# Create your views here.
+# 在自出接收请求 处理数据 返回响应
