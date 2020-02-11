babec1bcae66418c66c3a53dc52f6c7dfcf651f3
pm0303 <2641938505@qq.com>
2020-02-11 20:12:19 +0800

    测试

--- end/project/bookdemo/booktest/admin.py
+++ end/project/bookdemo/booktest/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 # Django自带的后台管理
+
+
+# 注册自己需要管理的模型
+from .models import Book, Hero
+
+admin.site.register(Book)
+admin.site.register(Hero)
--- end/project/bookdemo/booktest/models.py
+++ end/project/bookdemo/booktest/models.py
@@ -15,6 +15,10 @@ class Book(models.Model):
     pub_date = models.DateField(default="2020-02-11")
     price = models.FloatField(default=0)
 
+    def __str__(self):
+        return self.title
+
+
 class Hero(models.Model):
     """
     hero 继承Model 也可以操作数据库
@@ -24,3 +28,11 @@ class Hero(models.Model):
     content = models.CharField(max_length=100)
     # book 是一对多中的外键 on_delete代表删除主表数据是如何删除
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# django orm关联查询
+# 多方Hero   一方Book
+# 1多找一， 多方对象.关系字段    exp: h1.book
+# 2一找多， 一方对象.小写多方类名_set.all()   exp:  b1.hero_set.all()
